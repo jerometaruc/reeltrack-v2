e690dcd8c3efdb0c1ba84dbf852d88aaf99c9047
Jerome Taruc <jerometaruc29@gmail.com>
2025-08-05 14:29:00 -0700

    refactor(frontend): Change NavBar from const to function

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router';
 import { PlusIcon, ClapperboardIcon } from 'lucide-react';
 
-const NavBar = () => {
+function NavBar() {
     return (
         <header className="bg-base-300 border-b border-base-content/10">
             <div className="mx-auto max-w-6xl p-4">
