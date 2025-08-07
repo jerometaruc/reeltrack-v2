9f63e9dd0724e0e44792b66e1bba5957bc0472e3
Jerome Taruc <jerometaruc29@gmail.com>
2025-08-07 16:03:46 -0700

    feat(frontend): Add LoaderIcon animation to HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import type { Reel } from "../types/Reel";
 import NavBar from "../components/NavBar";
 import ReelsNotFound from "../components/ReelsNotFound";
 import ReelCard from "../components/ReelCard";
+import { LoaderIcon } from "lucide-react";
 
 const GET_REELS = gql`
     query GetReels {
@@ -28,8 +29,8 @@ function HomePage() {
         return (
             <div className="min-h-screen bg-base-200">
                 <NavBar />
-                <div className="max-w-6xl mx-auto p-4 mt-6">
-                    <div className="text-center text-primary py-10">Loading Reels...</div>
+                <div className="min-h-screen bg-base-200 flex items-center justify-center">
+                    <LoaderIcon className="animate-spin size-10" />
                 </div>
             </div>
         );
